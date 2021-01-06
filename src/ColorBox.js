0b22de90dd575fcc01774ecff55abfe5bf513406
Marwa Sh <marwa.al.shahal@gmail.com>
2021-01-06 13:26:30 +0000

    Automatically backed up by Learn

--- src/ColorBox.js
+++ src/ColorBox.js
@@ -10,8 +10,9 @@ export default class ColorBox extends Component {
 
   render() {
     return (
-      <div className="color-box" style={{opacity: null /*replace null with the value*/}}>
+      <div className="color-box" style={{opacity: {this.props.opacity}} /*replace null with the value*/}}>
         {/* your conditional code here! */}
+        {opacity >= 0.2 ? <div className="color-box" style={opacity:{this.props.opacity-1}}>: null}
       </div>
     )
   }
--- src/Comment.js
+++ src/Comment.js
@@ -1,1 +1,9 @@
-//your code here
+import React, { Component } from 'react';
+
+export default class X extends Component {
+  render(){
+    return (
+      <div className="comment"> {this.props.commentText} </div>
+      )
+  }
+}
